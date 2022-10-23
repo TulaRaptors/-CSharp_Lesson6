6b652e9c47785a91bad9759c3c0987b2987338ad
Sergey_Podgornov <psv_23@mail.ru>
2022-10-23 16:24:23 +0300

    Элементы с чётными индексами заменены на их квадраты

---
+++ Ex003_MatrixEvenIndexesSquare/Program.cs
@@ -0,0 +1,40 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+void FillMatrix(int[,] matrix, int minimum, int maximum)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = random.Next(minimum, maximum);
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+void ChangeEvenElements(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            if (i % 2 == 0 && j % 2 == 0)
+                matrix[i, j] = matrix[i, j] * matrix[i, j]; //Math.Pow(matrix[i,j],2);
+}
+
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int[rows, columns];
+FillMatrix(matrix, 1, 10);
+Console.WriteLine("Ваша матрица: ");
+PrintMatrix(matrix);
+ChangeEvenElements(matrix);
+Console.WriteLine("Обновленная матрица: ");
+PrintMatrix(matrix);
