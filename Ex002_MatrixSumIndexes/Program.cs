fc50dc61a519fbad1a0c959eca7ba508c67c3460
Sergey_Podgornov <psv_23@mail.ru>
2022-10-23 16:12:30 +0300

    Матрица где элемент равен сумме его индексов

---
+++ Ex002_MatrixSumIndexes/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = i+j;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int[rows, columns];
+FillMatrix(matrix);
+Console.WriteLine("Ваша матрица: ");
+PrintMatrix(matrix);
